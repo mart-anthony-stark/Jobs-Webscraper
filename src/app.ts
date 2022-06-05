d7c8acaac7f493ff5873156391c9bca82349d282
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-05 20:53:34 +0800

    First commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+import * as dotenv from "dotenv";
+dotenv.config({});
+import homeRoutes from "./routes/index";
+
+const server = fastify({logger: true});
+const PORT: number = parseInt(`${process.env.PORT}`, 10) || 8080;
+
+server.register(homeRoutes);
+
+server.listen(PORT, () => {
+  console.log(`Server started running on port ${PORT}`);
+});
