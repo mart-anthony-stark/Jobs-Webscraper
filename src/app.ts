f225c90cf7ed8e906d1532f651c311213f45f46a
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-05 21:57:29 +0800

    remove: logger

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config({});
 import homeRoutes from "./routes/index";
 
-const server = fastify({logger: true});
+const server = fastify();
 const PORT: number = parseInt(`${process.env.PORT}`, 10) || 8080;
 
 server.register(homeRoutes);
