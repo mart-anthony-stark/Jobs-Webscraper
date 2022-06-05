c098e910aad2112fc00599c02241f985ae92330b
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-05 22:25:01 +0800

    Add: salary and pagination

--- src/utils/jobseeker.ts
+++ src/utils/jobseeker.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-export const indeed = async (job: string, place: string) => {
+export const indeed = async (job: string, loc: string, page?: number) => {
+  page = page ? page * 10 : 0;
   const BASEURL: string = `https://ph.indeed.com`;
-  const URL: string = `${BASEURL}/jobs?q=${job || "all"}&l=${place || "all"}`;
+  const URL: string = `${BASEURL}/jobs?q=${job || "all"}&l=${
+    loc || "all"
+  }&start=${page}`;
   const selector: string = ".job_seen_beacon";
   const response = await axios.get(URL);
   const html = response.data;
@@ -15,12 +18,14 @@ export const indeed = async (job: string, place: string) => {
     const resultObj: any = {};
 
     const title = $(parentEl).find(".jobTitle a");
-    const companyName = $(parentEl).find(".companyName");
-    const companyLoc = $(parentEl).find(".companyLocation");
+    const companyName = $(parentEl).find(".companyName").text();
+    const companyLoc = $(parentEl).find(".companyLocation").text();
+    const salary = $(parentEl).find(".salary-snippet-container").text();
     resultObj["job_title"] = title.text();
     resultObj["link"] = `${BASEURL}${title.attr("href")}`;
-    resultObj["company_name"] = companyName.text();
-    resultObj["company_location"] = companyLoc.text();
+    resultObj["company_name"] = companyName;
+    resultObj["company_location"] = companyLoc;
+    resultObj["salary"] = salary;
 
     results.push(resultObj);
   });
