9cb0eb2815a1ba39cfb65536cd6315b7df0e88a6
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-05 21:57:49 +0800

    Add: findJob controller

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,5 +1,15 @@
+import axios from "axios";
+import cheerio from "cheerio";
+import { indeed } from "../utils/jobseeker";
+
 module.exports = {
   getAll: (req: any, reply: any) => {
     reply.send("Hello");
+  },
+
+  findJob: async(req: any, reply: any) => {
+    const { job, place } = req.query;
+    const indeedJobs = await indeed(job, place);
+    reply.send({ indeedJobs });
   },
 };
