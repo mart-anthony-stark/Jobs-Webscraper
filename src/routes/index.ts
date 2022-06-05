5aca50ac72d6a4e5802fc0ff10269109593ee814
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-05 21:57:54 +0800

    Add: routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 const controller = require("../controllers/home.controller");
 
 const route = (fastify: any, options: any, done: any) => {
-  fastify.get("/", controller.getAll);
+  fastify.get("/v1", controller.getAll);
+  fastify.get("/v1/jobs", controller.findJob);
   done();
 };
 
-export default route
+export default route;
